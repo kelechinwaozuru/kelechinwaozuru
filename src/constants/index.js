c9217750514a36346b0ec70e5820c3170cbb78f3
kelechinwaozuru <kelechi.danny1234@gmail.com>
2025-01-07 23:45:53 -0500

    OKUU

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,12 +6,12 @@ export const experiences = [
     iconBg: "",
     date: " May 2024 – August 2024",
     points: [
-      "Conducted comprehensive risk assessment, ensuring adherence to organizational compliance and regulatory standards .",
-      "Supported the development of risk models to optimize database systems, enhancing risk management efficiency.",
+      "Maintained database systems to track and analyse operational data contributing to data integrity for compliance reports.",
+      "Conducted comprehensive risk assessments with senior analysts aligning processes with regulatory and organizational standards.",
       "Streamlined data entry process, significantly reducing manual errors.",
       "Improved data entry and collection processes, ensuring data accuracy for regulatory purposes",
       "Collaborated with analysts to enhance the accuracy and reliability of data used for compliance reporting.",
-      "Assisted with presentation and reports, for the weekly task meeting.",
+      "Prepared detailed reports and presentations on organization posture for senior analysts.",
     ],
   },
   {
@@ -21,11 +21,11 @@ export const experiences = [
     iconBg: "",
     date: " April 2022 – Sept.2022 ",
     points: [
-      "Provided technical support for critical IT systems, ensuring continuity, minimal downtime and timely issue resolution.",
-      "Participated in weekly meetings,to identify opportunities for improving cybersecurity measures in engineering projects..",
+      "Assisted senior IT specialist to provide support for over 15+ employees across multiple locations, troubleshooting and resolving issues related to IT.",
+      "Participated in the successful implementation of technical support best practices, resulting in a 25% improvement in service quality and customer satisfaction.",
       "Collaborated with the IT team to validate project specifications and ensure alignment with client needs.",
-      "Expanded personal technical skillset through ongoing self -directed learning, attending workshops and participating in industry conferences.",
-      "Supported senior engineers in troubleshooting and resolving critical issues, ensuring projects milestones were met on time.",
+      "Expanded personal knowledge through ongoing self-directed learning, attending workshops and participating in industry conferences.",
+      "Supported senior engineers in delivering training programs to educate employees on IT policies reducing the number of support requests and increasing system up-time by 35%.",
     ],
   },
 ];
